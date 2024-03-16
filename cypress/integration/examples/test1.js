794770be2175985102913fbac1d84e2c95ab4f86
Muhammad Khaleed <mk@Muhammads-MBP.hsd1.pa.comcast.net>
2024-03-16 14:19:59 -0400

    checkbox radio functions select

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -15,13 +15,18 @@ describe('My First Test Suite', () => {
       const url = 'https://rahulshettyacademy.com/seleniumPractise/#/'
         cy.visit(url)
         cy.get('input.search-keyword').type('ca')
-        cy.wait(2000)
+       //ipmlicit wait it will always wait for whatever time you given
+        cy.wait(5000)
+        // explicit wait (10 seconds), only wait till element is visible or clickable
+        // will wait till element is visible or clickable, soon 
         cy.get('div.products div.product').should('have.length', 4)
         cy.get('div.product:visible').should('have.length', 4)
-        //parent child chaining
-        cy.get('.products').find('.product').should('have.length', 4)
+        //parent child chaining  // alias command
+        // const ab = "alias"
+        cy.get('.products').as('prodLocator');
+        cy.get('@prodLocator').find('.product').should('have.length', 4)
         //click + for index 3
-        cy.get('.products').find('.product').eq(2).find('.increment').click()
+        cy.get('@prodLocator').find('.product').eq(2).find('.increment').click()
         // two differen way locating element and it will click twice
         cy.get(':nth-child(3) > .product-action > button').click()
         cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
@@ -34,17 +39,26 @@ describe('My First Test Suite', () => {
         // veg === 'cashews' will fail return false
         // veg includes 'cashews' will pass return true
 
-        cy.get('.products').find('.product').each(($el, index, $list) => {
+        cy.get('@prodLocator').find('.product').each(($el, index, $list) => {
           const textVeg = $el.find('h4.product-name').text()
           cy.log(textVeg)
           if (textVeg.includes('Cashews')) {
             cy.wrap($el).find('button').click()
           }
         })
-        cy.log("Cypress World")
+
+        //non cypres function
+        // const logoo = cy.get('.brand').text();
+        // cy.log(logoo)
+        // cy.get('.brand').text
+
         // const brandName = cy.get('div.brand.greenLogo').text();
         // cy.log(brandName);
         // to resolve the promise we use then
+        // .text() is jquery command but it is non cypress command
+        //non cypress comman cannot resolve promise by themselves, we nee to manually
+        // resolve it by then()
+    
         cy.get('div.brand.greenLogo').then(function(logoElement) {
           const brandName = logoElement.text()
             cy.log(brandName)
@@ -52,13 +66,24 @@ describe('My First Test Suite', () => {
             cy.get('div.brand.greenLogo').should('include.text', 'EENKAR');
         })
 
+        //cypress directs run on your browser, cypress runner dev tools
+        console.log("Cypress World")
+        cy.log('Cypres World')
+        
+    
         cy.get('.brand').should('have.text', 'GREENKART')
         cy.get('.cart-icon > img').click()
-        cy.get('.action-block > button').contains('PROCEED TO CHECKOUT').click()
+        // cy.get('.action-block > button').contains('PROCEED TO CHECKOUT').click()
+        cy.contains('PROCEED TO CHECKOUT').click();
         cy.get('button').contains('Place Order').click()
         cy.get('select > option').should('have.length', 209)
         cy.get('select > option').should('have.length.gt', 208)
-        cy.get('select > option').should('have.length.lte', 209)
+        cy.get('select > option').should('have.length.lte', 209).then(function() {
+          console.log('Cypress World')
+          cy.log('Cypress World')
+        })
+
+        
 
 
         expect(true).to.equal(true)
--- cypress/integration/examples/test2.js
+++ cypress/integration/examples/test2.js
@@ -1,5 +1,37 @@
-describe('My Second Test Suite', () => {
-    it('My amazon test', () => {
-      cy.visit('https://www.amazon.com')
+
+describe('Automation Practice Test Suite', () => {
+    it('Different type of elements on page test', () => {
+      const url = "https://rahulshettyacademy.com/AutomationPractice/"
+      cy.visit(url)
+
+      cy.get('label[for="bmw"]').then(function(optText) {
+        const optTex1 = optText.text()
+          cy.log(optTex1)
+      })
+      
+      cy.get('#checkBoxOption1')
+    .check()
+    .should('be.checked')
+    .and('have.value', 'option1')
+    .get('label[for="bmw"]')
+    .should('contain', 'Option1');
+    
+      cy.get('#checkBoxOption2').should('not.be.checked')
+      cy.get('#checkBoxOption1').uncheck().should('not.be.checked').and('have.value', 'option1')
+      cy.get('#checkbox-example input').check(['option2', 'option3', 'option1'])
+      cy.get('select').select('option3')
+
+      //dynamic drop down
+      cy.get('#autocomplete').type('Ban')
+
+      // each funciton forloop, go through mulitiple elements on page
+      cy.get('.ui-menu-item div').each(($el, index, $list) => {
+        if ($el.text() === 'Bangladesh') {
+          cy.wrap($el).click();
+        }
+      })
+
+      cy.get('label[for="radio3"] input').click()
+      cy.get('label[for="radio2"] input').click()
     })
   })
